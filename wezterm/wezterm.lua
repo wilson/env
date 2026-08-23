5e51acfd5ed29ae01fe466174088cd3a83903c18
Wilson Bilkovich <wilson@wb78.net>
2026-08-23 10:30:27 -0400

    Correct WezTerm process matching, always open tabs in $HOME vs. CWD.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,28 +11,47 @@ config.color_scheme = 'Moonfly (Gogh)'
 
 config.audible_bell = 'Disabled'
 
-config.keys = {}
--- If we are in vim or nvim, map Cmd-w to close the current buffer
--- In other contexts, it behaves as normal, and closes the current WezTerm tab
--- This explicitly does not detect nvimdiff / nvimdiff2 at the moment
-local act = wezterm.action
-table.insert(config.keys, {
+-- Lua core lacks a "merge properties with object and return the new object" function.
+local function merge(t1, t2)
+  local newobj = {}
+  for k, v in pairs(t1) do newobj[k] = v end
+  for k, v in pairs(t2) do newobj[k] = v end
+  return newobj
+end
+
+-- Always open new tabs in ${HOME}, vs. using OSC 7 to inherit the CWD.
+local spawn_new_tab = {
+  key = 't',
+  action = wezterm.action.SpawnCommandInNewTab {
+    cwd = wezterm.home_dir,
+    domain = 'CurrentPaneDomain',
+  },
+}
+
+local close_tab = {
+  -- If we are in vim or nvim, map Cmd-w and Ctrl-Shift-w to close the current buffer
+  -- In other contexts, it behaves as normal, and closes the current WezTerm tab
   key = 'w',
-  mods = 'SUPER',
   action = wezterm.action_callback(function(window, pane)
     local process = pane:get_foreground_process_name()
-    if process and process:find('n?vim') ~= nil then
+    local basename = process and process:match('([^/\\]+)$')
+
+    -- This explicitly does not detect nvimdiff / nvimdiff2 at the moment
+    if basename == 'vim' or basename == 'nvim' then
       -- Just :bd, so we can't close unmodified buffers by accident
       pane:send_text('\x1b:bd\r')
     else
       -- Fallback to standard terminal behavior
-      window:perform_action(act.CloseCurrentTab { confirm = true }, pane)
+      window:perform_action(wezterm.action.CloseCurrentTab { confirm = true }, pane)
     end
   end),
-})
+}
 
--- TODO: These are potentially the defaults and could be omitted
-config.window_background_opacity = 1.0
-config.macos_window_background_blur = 0
+config.keys = {
+  merge({ mods = 'SUPER' }, spawn_new_tab),
+  merge({ mods = 'CTRL|SHIFT' }, spawn_new_tab),
+  merge({ mods = 'SUPER' }, close_tab),
+  merge({ mods = 'CTRL|SHIFT' }, close_tab),
+}
 
 return config
